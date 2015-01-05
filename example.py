50b1d6d1401d6ae786c3ea7b0112c02f02c9855c
Tristan Fisher <code@tristanfisher.com>
2015-01-05 20:08:36 +0300

    forgot to update example. done now, but not going to bump version on pypi

--- example.py
+++ example.py
@@ -3,8 +3,8 @@ from pprint import pprint
 
 pprint(easyos)
 
-if easyos['os'] == 'Darwin' and easyos['python_version'] == '2.7.8':
-    print "You're up to date!"
+if easyos['os'] == 'Darwin' and int(easyos['python_version'][0]) < 3:
+    print("Consider switching to Python 3 for your development machine.")
 
 with open (easyos['tmp_dir']+'/script', 'w') as log:
     message = "wow that's easy"
